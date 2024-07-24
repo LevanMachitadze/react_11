a29b970ea902e95c4ddb77630ca1393ca821c3c8
Levan <machitadzelevan@gmail.com>
2024-07-24 19:52:58 +0400

    finish

---
+++ src/pages/AboutPages.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPages = () => {
+  return (
+    <div>
+      <h1>AboutPages</h1>
+    </div>
+  );
+};
+
+export default AboutPages;
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <p>404 Error Page not found</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
